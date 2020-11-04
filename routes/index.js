4b3677332e7ac080ad6501b4a64f5f6d94337687
rasmus.gillberg <xxyaymanyayxx@hotmail.com>
2020-11-04 19:09:14 +0100

    Javascript fungerar som den ska

--- routes/index.js
+++ routes/index.js
@@ -15,27 +15,24 @@ router.get('/', function(req, res, next) {
   });
 });
 
-// router.get('/:id', async function (req, res, next) {
-//   try {
-//     const story = await query(
-//       'SELECT * FROM story WHERE id = ?',
-//       req.params.id
-//     );
-    
-//     const links = await query(
-//       'SELECT * FROM links WHERE story_id = ?',
-//       req.params.id
-//     );
-
-//     res.render('story',{
-//       status: 200,
-//       story: story,
-//       links: links
-//     });
-
-//   } catch (e) {
-//     console.error(e);
-//     next(e);
-//   }
-// });
+//  router.get('/:id', async function (req, res, next) {
+//    try {
+//      const story = await query(
+//        'SELECT * FROM story WHERE id = ?',
+//        req.params.id
+//      );
+//      const links = await query(
+//        'SELECT * FROM links WHERE story_id = ?',
+//        req.params.id
+     
+//      res.render('story',{
+//        status: 200,
+//        story: story,
+//        links: links
+     
+//    } catch (e) {
+//      console.error(e);
+//      next(e);
+//    }
+//  });
 module.exports = router;
